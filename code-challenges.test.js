ddbe7d430acbca94a23d1b04a71828f54abea940
Chris Lee <chrisyhlee@Chriss-MacBook-Pro.local>
2023-04-28 13:21:03 -0700

    example for week 2 assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -13,15 +13,47 @@
 // --------------------INSTRUCTOR EXAMPLE: Create a function that takes in an array of numbers and returns an array with all the numbers multiplied by 3.
 
 // Pseudo code:
+// input: array of numbers
+// output: array of all the numbers * 3
+// iterate through the array using .map since it will return an array for us
+// each value in the iteration needs to be multiplied by 3
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const numbersArray1 = [6, 7, 8, 9, 10]
-// Expected output: [18, 21, 24, 27, 30]
-const numbersArray2 = [24, 27, 30, 33, 36]
-// Expected output: [72, 81, 90, 99, 108]
+describe("exampleFunction", () => {
+    it("takes in an array of numbers and returns an array with all the numbers multiplied by 3", () => {
+        const numbersArray1 = [6, 7, 8, 9, 10]
+        // Expected output: [18, 21, 24, 27, 30]
+        const numbersArray2 = [24, 27, 30, 33, 36]
+        // Expected output: [72, 81, 90, 99, 108]   
+        expect(exampleFunction(numbersArray1)).toEqual([18, 21, 24, 27, 30])
+        expect(exampleFunction(numbersArray2)).toEqual([72, 81, 90, 99, 108])
+    })
+})
+
+// FAIL  ./code-challenges.test.js
+// exampleFunction
+//   ✕ takes in an array of numbers and returns an array with all the numbers multiplied by 3 (1 ms)
+
+// ● exampleFunction › takes in an array of numbers and returns an array with all the numbers multiplied by 3
+
+//   ReferenceError: exampleFunction is not defined
 
 // b) Create the function that makes the test pass.
+
+// const exampleFunction = (arr) => {
+//     return arr.map((num) => {
+//         return num * 3
+//     })
+// }
+
+// PASS  ./code-challenges.test.js
+// exampleFunction
+//   ✓ takes in an array of numbers and returns an array with all the numbers multiplied by 3 (1 ms)
+
+const exampleFunction = (arr) => {
+    return arr.map(num => num * 3)
+}
 
 // --------------------1) Create a function that takes a object as an argument and decides if the number inside it is evenly divisible by three or not.
 
